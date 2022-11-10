8bb4b2fed74f97f145fbbfd390c05e841b5c5091
Anandkonda765 <anand@vedaslabs.com>
2022-11-10 17:56:20 +0530

    Login Updated

--- app/src/main/java/com/example/sharedpreferenceswithsplash/Login.java
+++ app/src/main/java/com/example/sharedpreferenceswithsplash/Login.java
@@ -59,19 +59,13 @@ public class Login extends AppCompatActivity {
                         Toast.makeText(Login.this, "Entered Email  Was incorrect ", Toast.LENGTH_SHORT).show();
                     }
 
-
-
-
                     else if (!emailValue.equals(registeredEmail)) {
                         Toast.makeText(Login.this, "Entered email was incorrect", Toast.LENGTH_SHORT).show();
                     }else if(!passwordValue.equals(registeredPassword)){
                         Toast.makeText(Login.this, "Entered password was incorrect", Toast.LENGTH_SHORT).show();
                     }
-                    else if (emailValue != registeredEmail && passwordValue != registeredPassword) {
-                        Toast.makeText(Login.this, "If you want to login First Register With Us", Toast.LENGTH_SHORT).show();
-                    }
                 } else {
-                    Toast.makeText(Login.this, "Please enter Required Fields", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(Login.this, "If you want to login First Register With Us", Toast.LENGTH_SHORT).show();
 
                 }
 
